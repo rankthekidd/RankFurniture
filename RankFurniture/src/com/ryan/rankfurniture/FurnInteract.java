9a6be500eef25e8bc867718a98f8bfa417f68dd0
nnichael <playmogltoday@gmail.com>
2021-09-12 15:04:23 -0700

    Updated to 1.17.1

--- RankFurniture/src/com/ryan/rankfurniture/FurnInteract.java
+++ RankFurniture/src/com/ryan/rankfurniture/FurnInteract.java
@@ -76,8 +76,7 @@ public class FurnInteract implements Listener{
 				
 				if(stand == null) return;
 				//All furniture armor stands are tagged with "furniture." This is easy to see if the given furniture item 
-				//if(!stand.getScoreboardTags().contains("furniture")) return;
-				//TODO uncomment line above when MOGL switches to new world!!!
+				if(!stand.getScoreboardTags().contains("furniture")) return;
 				
 				if(fd == null) return;
 
